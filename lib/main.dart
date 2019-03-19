0285e18c9dcec907cec117f59bf17e2eafa6527d
Tran Phuc <glupstar312@gmail.com>
2019-03-19 21:49:48 +0100

    Android: Drawer + PopUpMenu (#4)
    * seperate into 2 layouts
    
    * add drawer tiles
    
    * add tile theme
    
    * add tile user
    
    * add PopUpMenu

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:aniflix/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
@@ -24,7 +23,7 @@ class MyApp extends StatelessWidget {
     return GraphQLProvider(
       client: client,
       child: PlatformApp(
-        home: MyHomePage(),
+        home: HomeScreen(),
         android: (_) => MaterialAppData(
               title: title,
               theme: ThemeData(
@@ -40,53 +39,6 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return PlatformWidget(
-      android: (_) => Scaffold(
-            appBar: AppBar(
-              title: Text('AniFlix'),
-            ),
-            body: HomeScreen(),
-            drawer: Drawer(),
-          ),
-      ios: (_) => CupertinoTabScaffold(
-            tabBar: _buildCupertinoTabBar(),
-            tabBuilder: (BuildContext context, int index) {
-              return CupertinoTabView(
-                builder: (BuildContext context) {
-                  return CupertinoPageScaffold(
-                    navigationBar: CupertinoNavigationBar(
-                      middle: Text('What is this'),
-                    ),
-                    child: HomeScreen(),
-                  );
-                },
-              );
-            },
-          ),
-    );
-  }
-
-  Widget _buildCupertinoTabBar() {
-    return CupertinoTabBar(
-      items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.tv)),
-        BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.bookOpen)),
-        BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.search)),
-        BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.user)),
-        BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.cog)),
-      ],
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-import 'package:aniflix/data/queries.dart' as queries;
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -12,26 +11,162 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Query(
-        options: QueryOptions(
-          document: queries.getAnimeIdAndTitle,
-          variables: {
-            'id': 1,
-          },
+    return PlatformWidget(
+      android: _buildAndroidHome,
+      ios: _buildIOSHome,
+    );
+  }
+
+  Widget _buildAndroidHome(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('AniFlix'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(text: 'Winter'),
+              Tab(text: 'Spring'),
+              Tab(text: 'Summer'),
+              Tab(text: 'Fall'),
+            ],
+          ),
+          actions: <Widget>[
+            _buildPopupMenu(),
+          ],
         ),
-        builder: (QueryResult result) {
-          if (result.errors != null) {
-            return Text(result.errors.toString());
-          }
-
-          if (result.loading) {
-            return CircularProgressIndicator();
-          }
-
-          return Text(result.data['Media']['title']['english']);
-        },
+        body: TabBarView(
+          children: <Widget>[
+            Text('Winter1'),
+            Text('Winter2'),
+            Text('Winter3'),
+            Text('Winter4'),
+          ],
+        ),
+        drawer: _buildDrawer(),
       ),
     );
   }
+
+  Widget _buildIOSHome(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: _buildCupertinoTabBar(),
+      tabBuilder: (BuildContext context, int index) {
+        return CupertinoTabView(
+          builder: (BuildContext context) {
+            return CupertinoPageScaffold(
+              navigationBar: CupertinoNavigationBar(
+                middle: Text('AniFlix'),
+              ),
+              child: Center(
+                child: Text('Wie die Homepage aussehen könnte'),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildCupertinoTabBar() {
+    return CupertinoTabBar(
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.tv)),
+        BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.bookOpen)),
+        BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.search)),
+        BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.user)),
+        BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.cog)),
+      ],
+    );
+  }
+
+  Widget _buildDrawer() {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            currentAccountPicture: CircleAvatar(
+              child: Text('DGlup3'),
+            ),
+            accountName: Text('Der User?'),
+            accountEmail: Text('XD'),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.tv),
+            title: Text('Anime'),
+            onTap: () {},
+            selected: true,
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.bookOpen),
+            title: Text('Manga'),
+            onTap: () {},
+            selected: false,
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.chartLine),
+            title: Text('Trending'),
+            onTap: () {},
+            selected: false,
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.edit),
+            title: Text('Reviews'),
+            onTap: () {},
+            selected: false,
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.newspaper),
+            title: Text('News'),
+            onTap: () {},
+            selected: false,
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.userAlt),
+            title: Text('User'),
+            onTap: () {},
+            selected: false,
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.tint),
+            title: Text('Theme'),
+            onTap: () {},
+            selected: false,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPopupMenu() {
+    return PopupMenuButton<MenuOption>(
+      onSelected: _onMenuClick,
+      itemBuilder: (BuildContext context) => <PopupMenuEntry<MenuOption>>[
+            PopupMenuItem<MenuOption>(
+              value: MenuOption.settings,
+              child: Text('Settings'),
+            ),
+            PopupMenuItem<MenuOption>(
+              value: MenuOption.donate,
+              child: Text('Donate'),
+            ),
+            PopupMenuItem<MenuOption>(
+              value: MenuOption.about,
+              child: Text('About'),
+            ),
+          ],
+    );
+  }
+
+  void _onMenuClick(MenuOption option) {
+    switch (option) {
+      case MenuOption.settings:
+        //TODO Navigate to Settings Screen
+        break;
+      default:
+        break;
+    }
+  }
 }
+
+enum MenuOption { settings, donate, about }
