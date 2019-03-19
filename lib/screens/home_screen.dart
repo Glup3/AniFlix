789e48260e0e7a7b244c4faff50f6cbf82836b3f
Phuc Tran <glup3.tran@gmail.com>
2019-03-19 20:39:12 +0100

    seperate into 2 layouts

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-import 'package:aniflix/data/queries.dart' as queries;
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -12,26 +11,67 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Query(
-        options: QueryOptions(
-          document: queries.getAnimeIdAndTitle,
-          variables: {
-            'id': 1,
-          },
+    return PlatformWidget(
+      android: _buildAndroidHome,
+      ios: _buildIOSHome,
+    );
+  }
+
+  Widget _buildAndroidHome(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('AniFlix'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(text: 'Winter'),
+              Tab(text: 'Spring'),
+              Tab(text: 'Summer'),
+              Tab(text: 'Fall'),
+            ],
+          ),
         ),
-        builder: (QueryResult result) {
-          if (result.errors != null) {
-            return Text(result.errors.toString());
-          }
-
-          if (result.loading) {
-            return CircularProgressIndicator();
-          }
-
-          return Text(result.data['Media']['title']['english']);
-        },
+        body: TabBarView(
+          children: <Widget>[
+            Text('Winter1'),
+            Text('Winter2'),
+            Text('Winter3'),
+            Text('Winter4'),
+          ],
+        ),
+        drawer: Drawer(),
       ),
+    );
+  }
+
+  Widget _buildIOSHome(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: _buildCupertinoTabBar(),
+      tabBuilder: (BuildContext context, int index) {
+        return CupertinoTabView(
+          builder: (BuildContext context) {
+            return CupertinoPageScaffold(
+              navigationBar: CupertinoNavigationBar(
+                middle: Text('AniFlix'),
+              ),
+              child: Center(child: Text('Wie die Homepage aussehen könnte'),),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildCupertinoTabBar() {
+    return CupertinoTabBar(
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.tv)),
+        BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.bookOpen)),
+        BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.search)),
+        BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.user)),
+        BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.cog)),
+      ],
     );
   }
 }
