e1d46c952d143ff2d77e7320b69996c4c44d30cc
Phuc Tran <glup3.tran@gmail.com>
2019-03-24 16:11:40 +0100

    remove unused fields

--- lib/data/queries.dart
+++ lib/data/queries.dart
@@ -1,13 +1,6 @@
 String getMediaOfSeason = """
   query(\$page: Int, \$perPage: Int, \$seasonYear: Int, \$season: MediaSeason, \$format: MediaFormat){
     Page(page: \$page, perPage: \$perPage){
-      pageInfo {
-        total
-        perPage
-        currentPage
-        lastPage
-        hasNextPage
-      }
       media(seasonYear: \$seasonYear, season: \$season, format: \$format) {
         id
         format
@@ -27,13 +20,6 @@ String getMediaOfSeason = """
 String getMostPopularManga = """
   query(\$page: Int, \$perPage: Int){
     Page(page: \$page, perPage: \$perPage){
-      pageInfo {
-        total
-        perPage
-        currentPage
-        lastPage
-        hasNextPage
-      }
       media(format: MANGA, sort: POPULARITY_DESC) {
         id
         popularity
